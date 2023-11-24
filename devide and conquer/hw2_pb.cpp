0196f102ac25ca05efbacad6545a5efde60d89ba
Zichen55 <90104520+David810209@users.noreply.github.com>
2023-11-24 15:15:18 +0800

    Create hw2_pb.cpp

---
+++ devide and conquer/hw2_pb.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define ll long long
+
+typedef vector<int> vi;
+ll total = 0;
+
+void merge(vi &arr, const int &l, const int &m, const int &r)
+{
+    int l_size = m - l + 1;
+    int r_size = r - m;
+    vi vl(l_size), vr(r_size);
+    int l_sum = 0;
+    for (int i = 0; i < l_size; i++)
+    {
+        vl[i] = arr[l + i];
+        l_sum += vl[i];
+    }
+    for (int i = 0; i < r_size; i++)
+    {
+        vr[i] = arr[m + i + 1];
+    }
+    int l_idx = 0, r_idx = 0, idx = l;
+    while (l_idx < l_size && r_idx < r_size)
+    {
+        if (vl[l_idx] < vr[r_idx])
+        {
+            arr[idx] = vr[r_idx];
+            total = total + (ll)(l_size - l_idx) * vr[r_idx] - l_sum;
+            idx++;
+            r_idx++;
+        }
+        else
+        {
+            arr[idx] = vl[l_idx];
+            l_sum -= vl[l_idx];
+            idx++;
+            l_idx++;
+        }
+    }
+    while (l_idx < l_size)
+    {
+        arr[idx++] = vl[l_idx++];
+    }
+    while (r_idx < r_size)
+    {
+        arr[idx++] = vr[r_idx++];
+    }
+}
+
+void merge_sort(vi &arr, const int &l, const int &r)
+{
+    if (l < r)
+    {
+        int m = (l + r) / 2;
+        merge_sort(arr, l, m);
+        merge_sort(arr, m + 1, r);
+        merge(arr, l, m, r);
+    }
+}
+
+signed main()
+{
+    ios::sync_with_stdio(0), cin.tie(0);
+    int n;
+    cin >> n;
+    vi arr(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    merge_sort(arr, 0, n - 1);
+    cout << total << '\n';
+    return 0;
+}
